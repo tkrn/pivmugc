5ce136ca365b70c56320138b8088544d55e50177
Michael Requeny <Michael.Requeny@chenmed.com>
2016-08-10 11:31:48 -0400

    Pull a random attendee name that has checked in

--- app/Admin.php
+++ app/Admin.php
@@ -1,99 +1,82 @@
 <?php
-
 class Admin {
-
-  private $title = 'PiVMUGc Administration';
-  private $content = 'admin.htm';
-  private $layout = 'layout-default.htm';
-
-  function DefaultDisplay($f3) {
-
-    $mapper = new DB\SQL\Mapper($f3->get('db'), 'admins');
-		$auth = new Auth($mapper, array('id'=>'username','pw'=>'password'));
-
-		// callback function because password is stored as an md5 hash.
-		function chkauth($pw) {
-		    return md5($pw);
-		}
-
-    if($auth->basic('chkauth')) {
-        $f3->set('title',$this->title);
-
-        if ($f3->get('SESSION.message_type') == 'success') {
-            $f3->set('jsnoty','jsnoty-success.htm');
-            $f3->set('jsnotymsg',$f3->get('SESSION.message'));
+    private $title = 'PiVMUGc Administration';
+    private $content = 'admin.htm';
+    private $layout = 'layout-default.htm';
+    function DefaultDisplay($f3) {
+        $mapper = new DB\SQL\Mapper($f3->get('db'), 'admins');
+        $auth = new Auth($mapper, array('id'=>'username','pw'=>'password'));
+        // callback function because password is stored as an md5 hash.
+        function chkauth($pw) {
+            return md5($pw);
         }
-        elseif ($f3->get('SESSION.message_type') == 'failure') {
-            $f3->set('jsnoty','jsnoty-error.htm');
-            $f3->set('jsnotymsg',$f3->get('SESSION.message'));
+        if($auth->basic('chkauth')) {
+            $f3->set('title',$this->title);
+            if ($f3->get('SESSION.message_type') == 'success') {
+                $f3->set('jsnoty','jsnoty-success.htm');
+                $f3->set('jsnotymsg',$f3->get('SESSION.message'));
+            }
+            elseif ($f3->get('SESSION.message_type') == 'failure') {
+                $f3->set('jsnoty','jsnoty-error.htm');
+                $f3->set('jsnotymsg',$f3->get('SESSION.message'));
+            }
+            $f3->clear('SESSION.message_type');
+            $f3->clear('SESSION.message');
+            $f3->set('content',$this->content);
+            echo \Template::instance()->render($this->layout);
         }
-
-        $f3->clear('SESSION.message_type');
-        $f3->clear('SESSION.message');
-
-        $f3->set('content',$this->content);
-        echo \Template::instance()->render($this->layout);
-      }
-
-  }
-
-  function ImportDatabase($f3) {
-    $ExcelImporter = new ExcelUtility();
-    $result = $ExcelImporter->ImportDatabase();
-
-    if ($result) {
-      $f3->set('SESSION.message_type','success');
-      $f3->set('SESSION.message','Successful data import!');
-    } else {
-      $f3->set('SESSION.message_type','failure');
-      $f3->set('SESSION.message','Failure to import! Please verify your file that was uploaded.');
     }
-
-    $f3->reroute('/admin');
-  }
-
-  function ExportDatabase($f3) {
-    $ExcelExporter = new ExcelUtility();
-    $ExcelExporter->ExportDatabase();
-  }
-
-  function OptimizeDatabase($f3) {
-    $result = $f3->get('db')->exec('VACUUM guests;');
-
-    if ($result > 0) {
-      $f3->set('SESSION.message_type','success');
-      $f3->set('SESSION.message','Successful database optimization');
-    } else {
-      $f3->set('SESSION.message_type','failure');
-      $f3->set('SESSION.message','Failure to optimizate database! Big time unknown error! Cleveland, we have a problem!');
+    function ImportDatabase($f3) {
+        $ExcelImporter = new ExcelUtility();
+        $result = $ExcelImporter->ImportDatabase();
+        if ($result) {
+            $f3->set('SESSION.message_type','success');
+            $f3->set('SESSION.message','Successful data import!');
+        } else {
+            $f3->set('SESSION.message_type','failure');
+            $f3->set('SESSION.message','Failure to import! Please verify your file that was uploaded.');
+        }
+        $f3->reroute('/admin');
     }
-
-    $f3->reroute('/admin');
-  }
-
-  function TruncateDatabase($f3) {
-    $result = $f3->get('db')->exec('DELETE FROM guests;');
-
-    if ($result > 0) {
-      $f3->set('SESSION.message_type','success');
-      $f3->set('SESSION.message','The guests table has been truncated!');
-    } else {
-      $f3->set('SESSION.message_type','failure');
-      $f3->set('SESSION.message','Failure to truncate! Big time unknown error! Cleveland, we have a problem!');
+    function RandomName($f3) {
+        // get the checked in user information
+        $result = $f3->get('db')->exec('SELECT * FROM guests WHERE timestamp NOT NULL ORDER BY Random()  LIMIT 1')[0];
+        $f3->set('SESSION.message_type','success');
+        $f3->set('SESSION.message',$result['firstname'].' '.$result['lastname'].' from: '.$result['company']);
+        $f3->reroute('/admin');
     }
-
-    $f3->reroute('/admin');
-  }
-
-  function Shutdown($f3) {
-     $cmdout = shell_exec('sudo shutdown -h now');
-  }
-
-  function PrintTestLabel($f3) {
-    $lblPrinter = new LabelPrinter();
-    $lblPrinter->PrintNameTagLabel("John Doe","Corporate America");
-
-    $f3->reroute('/admin');
-  }
-
+    function ExportDatabase($f3) {
+        $ExcelExporter = new ExcelUtility();
+        $ExcelExporter->ExportDatabase();
+    }
+    function OptimizeDatabase($f3) {
+        $result = $f3->get('db')->exec('VACUUM guests;');
+        if ($result > 0) {
+            $f3->set('SESSION.message_type','success');
+            $f3->set('SESSION.message','Successful database optimization');
+        } else {
+            $f3->set('SESSION.message_type','failure');
+            $f3->set('SESSION.message','Failure to optimizate database! Big time unknown error! Cleveland, we have a problem!');
+        }
+        $f3->reroute('/admin');
+    }
+    function TruncateDatabase($f3) {
+        $result = $f3->get('db')->exec('DELETE FROM guests;');
+        if ($result > 0) {
+            $f3->set('SESSION.message_type','success');
+            $f3->set('SESSION.message','The guests table has been truncated!');
+        } else {
+            $f3->set('SESSION.message_type','failure');
+            $f3->set('SESSION.message','Failure to truncate! Big time unknown error! Cleveland, we have a problem!');
+        }
+        $f3->reroute('/admin');
+    }
+    function Shutdown($f3) {
+        $cmdout = shell_exec('sudo shutdown -h now');
+    }
+    function PrintTestLabel($f3) {
+        $lblPrinter = new LabelPrinter();
+        $lblPrinter->PrintNameTagLabel("John Doe","Corporate America");
+        $f3->reroute('/admin');
+    }
 }
--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 // PiVMUGc - Version 2.1.1
-
 // Load 3rd party dependencies
 require('vendor/autoload.php');
-
 // Kickstart the framework
 $f3= Base::instance();
-
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9) {
 	trigger_error('PCRE version is out of date');
@@ -15,43 +12,36 @@ if ((float)PCRE_VERSION<7.9) {
 $f3->config('config.ini');
 $f3->set('AUTOLOAD','app/');
 $f3->set('db', new DB\SQL('sqlite:'.$f3->DB_PATH));
-
 // base index landing page
 $f3->route('GET /','Index->DefaultDisplay');
-
 // checkin
 $f3->route('GET /checkin','Checkin->DefaultDisplay');
 $f3->route('POST /checkin','Checkin->ProcessPOST');
-
 // register
 $f3->route('GET /register','Register->DefaultDisplay');
 $f3->route('POST /register','Register->ProcessPOST');
-
 // reprint
 $f3->route('GET /reprint','Reprint->DefaultDisplay');
 $f3->route('POST /reprint','Reprint->ProcessPOST');
-
 // admin
 $f3->route('GET /admin','Admin->DefaultDisplay');
 $f3->route('POST /admin/database/import','Admin->ImportDatabase');
 $f3->route('POST /admin/database/export','Admin->ExportDatabase');
+$f3->route('POST /admin/random/name','Admin->RandomName');
 $f3->route('POST /admin/database/optimize','Admin->OptimizeDatabase');
 $f3->route('POST /admin/database/truncate','Admin->TruncateDatabase');
 $f3->route('POST /admin/shutdown','Admin->Shutdown');
 $f3->route('POST /admin/print/test','Admin->PrintTestLabel');
-
 // set error pages
 $f3->set('ONERROR',function($f3){
 	if ($f3->get('ERROR.code') == '404') {
-	  echo \Template::instance()->render('layout-error-404.htm');
+		echo \Template::instance()->render('layout-error-404.htm');
 	} else {
 		echo \Template::instance()->render('layout-error.htm');
 	}
-
 	// clear just in case for any stale messages
 	$f3->clear('SESSION.message_type');
 	$f3->clear('SESSION.message');
 });
-
 // run this shit!
 $f3->run();
