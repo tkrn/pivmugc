5b8dbb0acd43f08119cbee8c9d669aa2d0fac261
Michael Requeny <Michael.Requeny@chenmed.com>
2016-08-10 10:48:01 -0400

    refactoring to support composer for 3rd party package dependencies

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-// PiVMUGc - Version 2.1
+// PiVMUGc - Version 2.1.1
+
+// Load 3rd party dependencies
+require('vendor/autoload.php');
 
 // Kickstart the framework
-$f3=require('lib/base.php');
+$f3= Base::instance();
 
 $f3->set('DEBUG',1);
-if ((float)PCRE_VERSION<7.9)
+if ((float)PCRE_VERSION<7.9) {
 	trigger_error('PCRE version is out of date');
-
+}
 // load configuration
 $f3->config('config.ini');
 $f3->set('AUTOLOAD','app/');
